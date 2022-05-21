d26167d5053e8364c44d57369f9cccc2feeb111c
shanu agrawal <agrawalshanu798@gmail.com>
2022-05-21 15:04:03 +0530

    FEAT: set background image after every refresh

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { UserOnBoard,Main } from "./screens/index";
 import {bg1} from "./Assets/index"
 import { useState } from "react";
+import { useBackgroundImage } from './hooks';
 
 function App() {
 const[userName,setUserName] = useState("")
 const[route,setRoute] = useState("onboard")
-  return <div style = {{background: `url(${bg1}) center center/cover no-repeat`}} className="App" >
+const {bgURL} = useBackgroundImage()
+  return <div style = {{background: `url(${bgURL}) center center/cover no-repeat`}} className="App" >
      { route === "onboard" && <UserOnBoard userName ={userName} setUserName ={setUserName} setRoute={setRoute}/>}
      { route === "main" && <Main userName ={userName}/>}
   </div>;
